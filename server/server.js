681c916d9993314dbad4613b07409b8671951639
Manish Rana <tripodindia01@gmail.com>
2022-05-17 10:03:04 +0530

    Server Added

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const cors = require('cors');
+const users = require('./users.json');
+
+app.use(express.json({limit: '50mb'}));
+
+app.use(express.json())
+app.use(cors({
+  origin :'*'
+}))
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
+  app.get("/",async(req,res)=>{
+    try {
+      res.json('Server running....')
+    } catch (err) {
+      console.log(err)
+      res.status(500).json(`ERROR : $${err.message}`)
+    }
+  })
+  const PORT = 8000;
+  app.listen(PORT,()=>{
+      console.log(`server running on ${PORT}`)
+  })
+
+  app.post("/add",async(req,res)=>{
+    try {
+      res.setHeader("Access-Control-Allow-Origin", "*")
+      let user = req.body;
+      console.log(user, 'user');
+      users.push(user);
+      fs.writeFile("users.json", JSON.stringify(users), err => {
+          console.log('User Added Successfully');
+      })
+    } catch (err) {
+      console.log(err);
+    }
+  })
+
--- src/features/users/userSlice.js
+++ src/features/users/userSlice.js
@@ -1,4 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const url = 'http://localhost:8000'
 
 const initialState = [];
 
@@ -8,6 +12,25 @@ const userSlice = createSlice({
   reducers: {
     addUser: (state, action) => {
       state.push(action.payload);
+      console.log(action, "state");
+      const options = {
+        url: `${url}/add`,
+        method: 'post',
+        data: {
+          id: action.payload.id,
+          firstname: action.payload.firstname,
+          lastname: action.payload.lastname,
+          group: action.payload.group,
+          permissions: action.payload.permissions,
+        },
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      axios(options)
+      .then(() => {
+        toast.success('User successfully created');
+      })
     },
     editUser: (state, action) => {
       const { id, firstname, lastname, group, permissions } = action.payload;
